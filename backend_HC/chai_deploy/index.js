10482fdec2ef47c2f0b0a6c66057ffabd44282b0
Aryan Giri <002giriaryan@gmail.com>
2025-08-25 20:18:07 +0530

    initial commit

---
+++ backend_HC/chai_deploy/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req,res)=>{
+    res.send('this is my twitter ID - AryanGiri_twts')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please Login Here!</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send("<a href='https://www.youtube.com/@chaiaurcode' target='_main'>Click on this to go to ChaiAurCode Youtube channel</a>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
