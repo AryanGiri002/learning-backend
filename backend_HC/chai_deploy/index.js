fd5849c891b609872ff12f2f81ca7437be9d60bd
Aryan Giri <002giriaryan@gmail.com>
2025-08-26 13:10:54 +0530

    using env variables

--- backend_HC/chai_deploy/index.js
+++ backend_HC/chai_deploy/index.js
@@ -1,6 +1,6 @@
+import 'dotenv/config';
 import express from "express"
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,6 +18,6 @@ app.get('/youtube',(req,res)=>{
     res.send("<a href='https://www.youtube.com/@chaiaurcode' target='_main'>Click on this to go to ChaiAurCode Youtube channel</a>")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
