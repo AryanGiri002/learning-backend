8311c55f60603d4026ba299ce04cb228c4346aa4
Aryan Giri <002giriaryan@gmail.com>
2025-09-04 22:38:16 +0530

    complete till step 3 of user Registration

--- backend_HC/01-project/src/controllers/user.controller.js
+++ backend_HC/01-project/src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiErrors.js"
+import {User} from "../models/user.model.js"
+
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
@@ -10,6 +13,28 @@ const registerUser = asyncHandler( async (req, res) => {
     // remove password and refresh token fields from response
     // check for user creation( whether the user was actually created or not)
     // if yes then return the response to the frontend
+
+
+    const {fullName, email, username, password} = req.body
+    console.log(fullName, email, username, password)
+
+    // if (fullName === "" ){
+    //     throw new ApiError(400 , "fullname is required")
+    // }
+
+    if ([fullName, email, username, password].some((field)=>{
+        return field?.trim() === ""
+    })) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [ { username } , { email } ]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with the following userName and Email already exists!")
+    }
 
 })
 
