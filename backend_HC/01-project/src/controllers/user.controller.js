f486acd97108d0f44f24cb4d58d132dff991259b
Aryan Giri <002giriaryan@gmail.com>
2025-09-04 21:51:58 +0530

    logic for user registration

--- backend_HC/01-project/src/controllers/user.controller.js
+++ backend_HC/01-project/src/controllers/user.controller.js
@@ -1,10 +1,16 @@
-import { response } from "express"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // get user details from frontend
+    // validation - not empty
+    // check if user alreday exists: username & email
+    // check for images , check for avatar (both are file uploads to our backend server)
+    // upload them to cloudinary , get the URL back
+    // create user object - create entry in DB
+    // remove password and refresh token fields from response
+    // check for user creation( whether the user was actually created or not)
+    // if yes then return the response to the frontend
+
 })
 
 export {registerUser}
